db7516ca6ba61678914448ead7c150c8fe67eb4b
Nyoto <nyoto.arif@ardomusnet.com>
2020-03-27 14:12:44 +0800

    DataBlockG: implementation & unit test

--- dlms/getDataResult.go
+++ dlms/getDataResult.go
@@ -2,6 +2,7 @@ package cosem
 
 import (
 	"bytes"
+	"encoding/hex"
 	. "gosem/axdr"
 )
 
@@ -39,6 +40,84 @@ func (dt *GetDataResult) Encode() []byte {
 		output.WriteByte(0x0)
 		value := dt.Value.(accessResultTag)
 		output.WriteByte(byte(value))
+	}
+
+	return output.Bytes()
+}
+
+// DataBlockG is DataBlock for the GET-response. Result must be either byte slice
+// or accessResultTag after creation, or else it will fail on Encode()
+type DataBlockG struct {
+	LastBlock   bool
+	BlockNumber uint32
+	IsResult    bool
+	Result      interface{}
+}
+
+func CreateDataBlockGAsData(lastBlock bool, blockNum uint32, result interface{}) *DataBlockG {
+	switch res := result.(type) {
+	case string:
+		bt, e := hex.DecodeString(res)
+		if e != nil {
+			panic(e)
+		}
+		return &DataBlockG{lastBlock, blockNum, false, bt}
+
+	case []byte:
+		return &DataBlockG{lastBlock, blockNum, false, res}
+
+	default:
+		panic("CreateDataBlockGAsData result only accept hexstring or byte slice.")
+	}
+}
+
+func CreateDataBlockGAsResult(lastBlock bool, blockNum uint32, result accessResultTag) *DataBlockG {
+	return &DataBlockG{lastBlock, blockNum, true, result}
+}
+
+func CreateDataBlockG(lastBlock bool, blockNum uint32, result interface{}) *DataBlockG {
+	switch res := result.(type) {
+	case string:
+		bt, e := hex.DecodeString(res)
+		if e != nil {
+			panic(e)
+		}
+		return CreateDataBlockGAsData(lastBlock, blockNum, bt)
+
+	case []byte:
+		return CreateDataBlockGAsData(lastBlock, blockNum, res)
+
+	case accessResultTag:
+		return CreateDataBlockGAsResult(lastBlock, blockNum, res)
+
+	default:
+		panic("DataBlockG result only accept hexstring, byte slice, or DataAccessResult.")
+	}
+}
+
+func (dt *DataBlockG) Encode() []byte {
+	var output bytes.Buffer
+
+	if dt.LastBlock {
+		output.WriteByte(0x1)
+	} else {
+		output.WriteByte(0x0)
+	}
+
+	blk, e := EncodeDoubleLongUnsigned(dt.BlockNumber)
+	if e != nil {
+		panic(e)
+	}
+	output.Write(blk)
+
+	if dt.IsResult == true {
+		output.WriteByte(0x1)
+		value := dt.Result.(accessResultTag)
+		output.WriteByte(byte(value))
+	} else {
+		output.WriteByte(0x0)
+		value := dt.Result.([]byte)
+		output.Write(value)
 	}
 
 	return output.Bytes()
--- dlms/getDataResult_test.go
+++ dlms/getDataResult_test.go
@@ -71,3 +71,75 @@ func TestGetDataResult(t *testing.T) {
 	var c GetDataResult = *CreateGetDataResult(999)
 	c.Encode()
 }
+
+func TestDataBlockGAsData(t *testing.T) {
+	var a DataBlockG = *CreateDataBlockGAsData(true, 1, "07D20C04030A060BFF007800")
+
+	t1 := a.Encode()
+	result := []byte{1, 0, 0, 0, 1, 0, 7, 210, 12, 4, 3, 10, 6, 11, 255, 0, 120, 0}
+
+	res := bytes.Compare(t1, result)
+	if res != 0 {
+		t.Errorf("t1 Failed. get: %d, should:%v", t1, result)
+	}
+
+	var b DataBlockG = *CreateDataBlockGAsData(true, 1, []byte{1, 0, 0, 3, 0, 255})
+	t2 := b.Encode()
+	result = []byte{1, 0, 0, 0, 1, 0, 1, 0, 0, 3, 0, 255}
+
+	res = bytes.Compare(t2, result)
+	if res != 0 {
+		t.Errorf("t2 Failed. get: %d, should:%v", t2, result)
+	}
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("t3 should've panic on wrong Value")
+		}
+	}()
+	var c DataBlockG = *CreateDataBlockGAsData(true, 1, TagAccSuccess)
+	c.Encode()
+}
+
+func TestDataBlockGAsResult(t *testing.T) {
+	var a DataBlockG = *CreateDataBlockGAsResult(true, 1, TagAccSuccess)
+
+	t1 := a.Encode()
+	result := []byte{1, 0, 0, 0, 1, 1, 0}
+
+	res := bytes.Compare(t1, result)
+	if res != 0 {
+		t.Errorf("t1 Failed. get: %d, should:%v", t1, result)
+	}
+}
+
+func TestDataBlockG(t *testing.T) {
+	var a DataBlockG = *CreateDataBlockG(true, 1, "07D20C04030A060BFF007800")
+
+	t1 := a.Encode()
+	result := []byte{1, 0, 0, 0, 1, 0, 7, 210, 12, 4, 3, 10, 6, 11, 255, 0, 120, 0}
+
+	res := bytes.Compare(t1, result)
+	if res != 0 {
+		t.Errorf("t1 Failed. get: %d, should:%v", t1, result)
+	}
+
+	var b DataBlockG = *CreateDataBlockG(true, 1, []byte{1, 0, 0, 3, 0, 255})
+	t2 := b.Encode()
+	result = []byte{1, 0, 0, 0, 1, 0, 1, 0, 0, 3, 0, 255}
+
+	res = bytes.Compare(t2, result)
+	if res != 0 {
+		t.Errorf("t2 Failed. get: %d, should:%v", t2, result)
+	}
+
+	var c DataBlockG = *CreateDataBlockG(true, 1, TagAccSuccess)
+
+	t3 := c.Encode()
+	result = []byte{1, 0, 0, 0, 1, 1, 0}
+
+	res = bytes.Compare(t3, result)
+	if res != 0 {
+		t.Errorf("t3 Failed. get: %d, should:%v", t3, result)
+	}
+}
